9f3930575cce714f712d81bdac911c97a0ca6678
Freddyp666 <pat-.-freddy@outlook.com>
2025-07-03 23:52:08 -0500

    Show date weather in the dev

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-let api_key='a62031a7145b770609c8db06b823546e';
-
-let api_url = 'https://api.openweathermap.org/data/2.5/weather';
-
-let city = 'London';
-
-let url = `${api_url}?q=${city}&appid=${api_key}`;
+let api_key = "a62031a7145b770609c8db06b823546e";
+let url_base = "https://api.openweathermap.org/data/2.5/weather";
 
 
-fetch(url)
-    .then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
+
+document.getElementById('btnSearch').addEventListener('click', () => {
+    const city = document.getElementById("inputCity").value;
+    if (city) {
+        fetchWeatherDate(city);
+    }
+});
+
+function fetchWeatherDate(city) {
+    let urlFull = `${url_base}?q=${city}&appid=${api_key}`;
+    console.log(urlFull);
+
+    fetch(urlFull)
+        .then((response) => response.json())
+        .then((response) => showWeatherData(response))
+        .catch((error) => console.error("Error fetching weather data:", error));
+}
+
+function showWeatherData(data) {
+    console.log(data);
+    const divWeatherDate = document.getElementById("weatherDate");
+
+    divWeatherDate.innerHTML = "";
+    const cityName = data.name;
+    const temperature = data.main.temp;
+    const weatherDescription = data.weather[0].description;
+    const humidity = data.main.humidity;
+
+    divWeatherDate.innerHTML = `
+        <h2>Clima en ${cityName}</h2>
+        <p>Temperatura: ${(temperature - 273.15).toFixed(2)} °C</p>
+        <p>Descripción: ${weatherDescription}</p>
+        <p>Humedad: ${humidity}%</p>
+    `;
+}
